e6319ea521bd2ff68a0f7cb12df146a440357fa6
abrarfrahman <abrarfrahman@gmail.com>
2024-11-12 09:35:57 -0800

    restore node_env and remove duplic code

--- next.config.js
+++ next.config.js
@@ -4,25 +4,10 @@ const nextConfig = {
     return [
       {
         source: "/api/py/:path*",
-        destination: "http://127.0.0.1:8000/api/:path*",
-      },
-      {
-        source: "/api/:path*",
-        destination: "/api/py/:path*",
-      },
-      {
-        source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/docs"
-            : "/api/py/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/openapi.json"
-            : "/api/py/openapi.json",
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/:path*",
       },
     ];
   },
@@ -35,11 +20,10 @@ const nextConfig = {
           { key: "Access-Control-Allow-Origin", value: "https://abrartisan-ai.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
-
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
