3495ab4698e89e897151f9a998fa09f6da5ac0c8
abrarfrahman <abrarfrahman@gmail.com>
2024-11-12 09:50:05 -0800

    add another /api/

--- next.config.js
+++ next.config.js
@@ -6,8 +6,12 @@ const nextConfig = {
         source: "/api/py/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
+            ? "http://127.0.0.1:8000/:path*"
             : "/api/:path*",
+      },
+      {
+        source: "/api/:path*",
+        destination: "/api/py/:path*",
       },
     ];
   },
